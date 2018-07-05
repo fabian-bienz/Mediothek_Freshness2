98cfc0380fbb3184451803e3baacfdc1b94ebc44
fabian bienz <fabian-bienz@users.noreply.github.com>
2018-07-05 09:29:16 +0200

    added AudioBuilder

--- src/Mediothek/Mediothek.java
+++ src/Mediothek/Mediothek.java
@@ -1,19 +1,36 @@
 package Mediothek;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Mediothek {
 	
 	private List kunden;
-	private List medien;
+	private Map<Integer, String> medien;
 	
 	public Mediothek() {
 		kunden = new ArrayList<Kunde>();
-		medien = new ArrayList<Medien>();
+		medien = new HashMap<Integer, String>();
+	}
+	
+	public void addMedium(Medien medium) {
+		medien.put(medium.getId(),medium.getTitel());
+	}
+	
+	public void printMedien() {
+		for(Map.Entry m:medien.entrySet()){
+			System.out.print(m.getKey());
+			System.out.print(": " + m.getValue() + "\n");
+		}
 	}
 	
 	
+	public void registerKunde() {
+		Kunde kunde = new Kunde();
+		kunden.add(kunde);
+	}
 	
 	
 	
--- src/Mediothek/Mitgliedschaft.java
+++ src/Mediothek/Mitgliedschaft.java
@@ -1,4 +1,15 @@
 package Mediothek;
 
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
 public class Mitgliedschaft {
+	private Date endDatum;
+	
+	public Mitgliedschaft() {
+		endDatum = new Date();
+		endDatum = new Date(endDatum.getTime()+ TimeUnit.DAYS.toMillis(30));
+	}
+	
+	
 }
